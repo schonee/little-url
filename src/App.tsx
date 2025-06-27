2dfdb0db83dc615e3eedc83a423143cdb8d1aa5c
Thalia Schöne <thalia.schone@gmail.com>
2025-06-27 18:33:17 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <h1>Little URL</h1>
+      <div className="card">
+        <input className="url-input" type="text" placeholder="Insert big URL" />
+        <button className="url-button" onClick={() => {}}>
+          Make it Little
+        </button>
+      </div>
+      <footer className="read-the-docs">Made by Thalia Schöne</footer>
+    </>
+  );
+}
+
+export default App;
